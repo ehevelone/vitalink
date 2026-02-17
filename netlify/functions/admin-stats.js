421d802d16b077d5eef9d5b89ce70d0de6581a27
Eric <ehevelone@gmail.com>
2026-02-17 14:04:27 -0600

    Remove site functions and switch admin to app backend

--- netlify/functions/admin-stats.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "Admin stats function is working."
-    })
-  };
-};
